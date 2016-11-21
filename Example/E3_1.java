7c629498c5f4ac5910014cc9a4117351283a9a68
Li Kunhao <372383419@qq.com>
2016-11-21 14:49:41 +0800

    Update and rename 3_1.java to Example/E3_1.java

--- Example/E3_1.java
+++ Example/E3_1.java
@@ -36,7 +36,7 @@ class A implements I
     }
 }
 
-public class 3_1{
+public class E3_1{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         double y=0,x=0;
