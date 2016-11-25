fa2956804a73ce923ca96a2ffcaa487760f99411
Li Kunhao <372383419@qq.com>
2016-11-25 14:24:09 +0800

    Add files via upload

---
+++ Example/E10_9.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class E10_9
+{
+    public static void main(String args[])
+    {
+        PoliceWindow policeWin=new PoliceWindow();
+        
+    }
+}
+
+class PoliceWindow extends JFrame implements ActionListener
+{
+    JTextField titleText;
+    JPasswordField passwordText;
+    PoliceWindow()
+    {
+        titleText=new JTextField(10);
+        passwordText=new JPasswordField(10);
+        passwordText.setEchoChar('*');
+        
+        titleText.addActionListener(this);
+        passwordText.addActionListener(this);
+
+        setLayout(new FlowLayout());
+        add(titleText);
+        add(passwordText);
+
+        setBounds(200,100,200,150);
+        setVisible(true);
+        validate();
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        JTextField textSource=(JTextField)e.getSource();
+        
+        if(textSource==titleText)
+        {
+            this.setTitle(titleText.getText());
+        }
+        else if(textSource==passwordText)
+        {
+            char c[]=passwordText.getPassword();
+            titleText.setText(new String(c));
+        }
+    }
+}
