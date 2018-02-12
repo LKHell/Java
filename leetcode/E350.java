64ffc84aae6308700720f48a30ec7faf676d8e81
Li Kunhao <372383419@qq.com>
2018-02-12 19:28:15 +0800

    day 14 part1

--- leetcode/E350.java
+++ leetcode/E350.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 // Given two arrays, write a function to compute their intersection.
 
 // Example:
@@ -5,6 +9,28 @@
 
 class Solution {
   public int[] intersect(int[] nums1, int[] nums2) {
-      
+      Map<Integer,Integer> map = new HashMap<>();
+      ArrayList<Integer> result_list = new ArrayList<>();
+
+      for (int i=0; i<nums1.length; i++) {
+        if(map.containsKey(nums1[i])) {
+          map.put(nums1[i], map.get(nums1[i])+1);
+        }
+        else {
+          map.put(nums1[i], 1);
+        }
+      }
+      for(int i = 0; i<nums2.length; i++) {
+        if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+          result_list.add(nums2[i]);
+          map.put(nums2[i], map.get(nums2[i])-1);
+        }
+      }
+
+      int [] ans = new int[result_list.size()];
+      for(int i = 0; i<ans.length; i++) {
+        ans[i] = result_list.get(i);
+      }
+      return ans;
   }
 }
