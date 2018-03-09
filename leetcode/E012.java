57c4c45b1a964301cdeb8d72f65afe9ef4604081
Li Kunhao <372383419@qq.com>
2018-03-09 23:18:04 +0800

    add 1 solution

--- leetcode/E012.java
+++ leetcode/E012.java
@@ -25,3 +25,21 @@ class Solution {
       return res;
   }
 }
+
+//
+class Solution {
+  private static int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  private static String[] strings = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+  public String intToRoman(int num) {
+      StringBuilder res = new StringBuilder();
+      for (int i  = 0 ; i<nums.length; i++) {
+          while (num >= nums[i]) {
+              res.append(strings[i]);
+              num -= nums[i];
+          }
+      }
+
+      return res.toString();
+  }
+}
